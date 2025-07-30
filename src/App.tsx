8c7cafab0d13f8b1d06c44b9dfd0121f7379bc77
陳思瑜 Judy <laplus.space@gmail.com>
2025-07-30 21:35:16 +0800

    feat(src/components): 新增 Copyright

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import About from './components/About';
-import Contact from './components/Contact';
-import Contributions from './components/Contributions';
-import Experience from './components/Experience';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Contributions from "./components/Contributions";
+import Copyright from "./components/Copyright";
+import Experience from "./components/Experience";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Contributions />
         <Contact />
       </main>
+      <Copyright />
     </div>
   );
 }
---
+++ src/components/Copyright.tsx
@@ -0,0 +1,7 @@
+const Copyright = () => (
+  <footer className="w-full flex justify-center items-center py-4 px-8">
+    <p>© 2025 Margo Chen | Built with React and Tailwind</p>
+  </footer>
+);
+
+export default Copyright;
