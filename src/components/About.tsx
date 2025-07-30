a4fca9f21b4ef2980409a0e3aa20e35361d9b152
陳思瑜 Judy <laplus.space@gmail.com>
2025-07-30 23:27:37 +0800

    feat(src/components): About 新增 Codewars 連結 以及 調整 flex order

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,27 +9,34 @@ const About = () => (
           href="https://github.com/laplus-x"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-gray-600 hover:text-[#ea97ef] transition-colors p-2 rounded-full border"
           aria-label="GitHub"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
-            <path d="M9 18c-4.51 2-5-2-7-2"></path>
-          </svg>
+          <img
+            alt="GitHub"
+            height={36}
+            width={36}
+            src="https://github.com/fluidicon.png"
+          />
+        </a>
+
+        <a
+          href="https://www.codewars.com/users/laplus-x"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Codewars"
+        >
+          <img
+            alt="Codewars"
+            src="https://www.codewars.com/packs/assets/logo.f607a0fb.svg"
+          />
         </a>
       </div>
     </div>
     <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-6 gap-4">
-      <ul className="list-disc pl-6 space-y-2" style={{ color: "#fbfefe" }}>
+      <ul
+        className="list-disc pl-6 space-y-2 order-2 sm:order-1"
+        style={{ color: "#fbfefe" }}
+      >
         <li>積極溝通，減少認知的誤差，避免做白工</li>
         <li>透明化工作資訊，讓團隊掌握現況</li>
         <li>推動自動化程式，優化作業流程，減輕團隊的工作量</li>
@@ -38,11 +45,13 @@ const About = () => (
         <li>富有責任感，盡己所能達成目標</li>
         <li>產能穩定，精準估時</li>
       </ul>
-      <img
-        src="https://avatars.githubusercontent.com/u/105040809"
-        alt="avatar"
-        className="w-50 h-50 sm:ml-4 shadow-lg bg-[#050b1e] mb-2 sm:mb-0 rounded-lg object-cover"
-      />
+      <div className="flex justify-center sm:ml-4 mb-2 sm:mb-0 order-1 sm:order-2">
+        <img
+          src="https://avatars.githubusercontent.com/u/105040809"
+          alt="avatar"
+          className="w-50 h-50 shadow-lg bg-[#050b1e] rounded-lg object-cover"
+        />
+      </div>
     </div>
   </section>
 );
